835783bc71703fe17eb50889fae165fb154979c5
geots <77642975+Geots@users.noreply.github.com>
2022-11-07 10:27:04 +0000

    anim

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ function togNotf() {
         down = true;
     }
 }
+
+var i = 0;
+var txt = 'Lorem ipsum dummy text blabla.';
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("txt").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
